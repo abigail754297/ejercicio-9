db0e53c176cdca1d68a42a321d316eed4e8c9e14
Estudiante <abi>
2025-06-20 17:31:17 -0500

    Agregar archivos de proyecto.

---
+++ ejercicio 9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nota;
+            Console.WriteLine("ingrese una nota:");
+            nota = Convert.ToInt32(Console.ReadLine()); 
+
+            while (nota<0 || nota > 20) 
+            {
+                Console.WriteLine("el valor es incorrecto");
+                Console.WriteLine("ingrese una nota entre 0 y 20:");
+                nota = Convert.ToInt32(Console.ReadLine());
+            }
+            if (nota >=16)
+            {
+                Console.WriteLine("excelente");
+            }
+        }
+    }
+}
